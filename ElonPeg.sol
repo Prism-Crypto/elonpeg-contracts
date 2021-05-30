abe1c94fe872513b2f07a6f4f1ce77f30331733a
sion42x <82188389+sion42x@users.noreply.github.com>
2021-05-30 18:40:22 -0400

    tTotal Fixes Final

--- ElonPeg.sol
+++ ElonPeg.sol
@@ -763,7 +763,7 @@ interface IUniswapV2Router02 is IUniswapV2Router01 {
     ) external;
 }
 
-contract Abrqsstrv is Context, IERC20, Ownable {
+contract ElonPeg is Context, IERC20, Ownable {
     using SafeMath for uint256;
     using Address for address;
 
